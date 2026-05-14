0936242799a2b72c7b22e3e0efb6be69311de651
alireza javadi <alirezajavadigit@gmail.com>
2026-05-14 21:49:12 +0330

    fix(request): remove dns email validation to prevent docker timeout in tests

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -17,7 +17,7 @@ class LoginRequest extends FormRequest
             'email' => [
                 'required',
                 'string',
-                'email:rfc,dns',
+                'email:rfc',
                 'max:255',
             ],
             'password' => [
